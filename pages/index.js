e48da2f722e8d09b15457772b7f0769774b207a9
radeljfc <radeljfc@icloud.com>
2026-04-14 02:04:10 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
       <h1>Prueba tu estilo con </h1>
       <h1>VESTA </h1> <h2>Estilo para Ellos </h2>
       <a href="/probar">Ir a probar</a>
+  <p><i>Hasta ahora en este DEMO, solo podrás tomar o subir una foto y devolveremos la misma como resultado</i></p>
     </div>
   );
 }
