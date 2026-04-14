eefb66ac35f6d1b4aef15fde81a0535cac7bcb4b
radeljfc <radeljfc@icloud.com>
2026-04-14 02:33:13 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: 20 }}>
       <h1>Prueba tu estilo con </h1>
-      <h1>VESTA </h1> <h2>Estilo para Ellos </h2>
+      <h1>VESTA </h1> <h2>Estilo para Ellosss </h2>
       <a href="/probar">Ir a probar</a>
   <p><i>Hasta ahora en este DEMO, solo podrás tomar o subir una foto y devolveremos la misma como resultado</i></p>
     </div>
