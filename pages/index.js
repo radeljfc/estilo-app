d1f738d31e47d826fa5abd9078699f8339c81b16
radeljfc <radeljfc@icloud.com>
2026-04-13 23:29:03 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div style={{ padding: 20 }}>
-      <h1>Prueba tu estilo</h1>
-      <a href="/probar">Ir a probar</a>
+      <h1>Prueba tu estilooo</h1>
+      <a href="/probar">Ir a probaaar</a>
     </div>
   );
 }
