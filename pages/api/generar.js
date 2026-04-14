6fae447180b0865e75ca7c565c55a6623ad9055c
radeljfc <radeljfc@icloud.com>
2026-04-14 16:46:48 -0500

    Update generarresult.js

--- pages/api/generar.js
+++ pages/api/generar.js
@@ -5,8 +5,15 @@ export default async function handler(req, res) {
     console.log("Imagen recibida:", imageUrl);
 
     return res.status(200).json({
-      image: imageUrl
-    });
+  image: imageUrl,
+  estilo: "Urban Street",
+  prendas: [
+    "Hoodie negro oversized",
+    "Jeans slim fit",
+    "Zapatillas blancas",
+    "Cadena plateada"
+  ]
+});
 
   } catch (error) {
     return res.status(500).json({ error: "error" });
