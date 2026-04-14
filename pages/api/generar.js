bb84738113705ad417263f316f9c3a8b500b387b
radeljfc <radeljfc@icloud.com>
2026-04-13 21:05:13 -0500

    backend recibe imagen

--- pages/api/generar.js
+++ pages/api/generar.js
@@ -1,5 +1,14 @@
 export default async function handler(req, res) {
-  return res.status(200).json({
-    image: "https://picsum.photos/300/400?random=" + Math.random()
-  });
+  try {
+    const { image } = req.body;
+
+    console.log("imagen recibida:", image ? "sí" : "no");
+
+    return res.status(200).json({
+      image: "https://picsum.photos/300/400?random=" + Math.random()
+    });
+
+  } catch (error) {
+    return res.status(500).json({ error: "error" });
+  }
 }
