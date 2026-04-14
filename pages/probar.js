9235257d32acebb46b2750a3035a0b72e4c5da8c
radeljfc <radeljfc@icloud.com>
2026-04-14 01:29:57 -0500

    Update probar2.js

--- pages/probar.js
+++ pages/probar.js
@@ -1,36 +1,75 @@
 import { useState } from "react";
 
 export default function Probar() {
+  const [imageBase64, setImageBase64] = useState(null);
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const enviar = async () => {
-    try {
-      setLoading(true);
+  const handleImage = (file) => {
+    const reader = new FileReader();
 
-      const res = await fetch("/api/generar", {
-        method: "POST"
-      });
+    reader.onloadend = () => {
+      setImageBase64(reader.result);
+    };
 
-      const data = await res.json();
-
-      if (data.image) {
-        setResult(data.image);
-      } else {
-        alert("No se generó imagen");
-      }
-
-    } catch (error) {
-      console.error(error);
-      alert("Error");
-    } finally {
-      setLoading(false);
+    if (file) {
+      reader.readAsDataURL(file);
     }
   };
+
+  const enviar = async () => {
+  try {
+    setLoading(true);
+
+    // 1. Subir imagen a Cloudinary
+    const formData = new FormData();
+    formData.append("file", document.querySelector('input[type="file"]').files[0]);
+    formData.append("upload_preset", "Estiloapp");
+
+    const cloudRes = await fetch(
+      "https://api.cloudinary.com/v1_1/djk1h8mkc/image/upload",
+      {
+        method: "POST",
+        body: formData
+      }
+    );
+
+    const cloudData = await cloudRes.json();
+
+    const imageUrl = cloudData.secure_url;
+
+    // 2. Enviar URL al backend
+    const res = await fetch("/api/generar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        imageUrl
+      })
+    });
+
+    const data = await res.json();
+    setResult(data.image);
+
+  } catch (error) {
+    console.error(error);
+    alert("error");
+  } finally {
+    setLoading(false);
+  }
+};
 
   return (
     <div style={{ padding: 20 }}>
       <h2>Prueba tu estilo</h2>
+
+      <input 
+        type="file" 
+        onChange={(e) => handleImage(e.target.files[0])}
+      />
+
+      <br /><br />
 
       <button onClick={enviar}>
         {loading ? "Generando..." : "Generar estilo"}
