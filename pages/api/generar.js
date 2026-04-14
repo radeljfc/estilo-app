8b843425d62fab497951f1ab8ef5c76067dcd0af
radeljfc <radeljfc@icloud.com>
2026-04-14 00:11:22 -0500

    Update generar.js

--- pages/api/generar.js
+++ pages/api/generar.js
@@ -1,14 +1,5 @@
 export default async function handler(req, res) {
-  try {
-    const { image } = req.body;
-
-    console.log("imagen recibida:", image ? "sí" : "no");
-
-    return res.status(200).json({
-      image: "https://picsum.photos/300/400?random=" + Math.random()
-    });
-
-  } catch (error) {
-    return res.status(500).json({ error: "error" });
-  }
+  res.status(200).json({
+    image: "https://placehold.co/300x400"
+  });
 }
