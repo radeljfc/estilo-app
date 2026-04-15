e4bb60486dd4b749ae224d1d370afd6d20acb7c3
radeljfc <radeljfc@icloud.com>
2026-04-14 21:03:58 -0500

    Update generar.js

--- pages/api/generar.js
+++ pages/api/generar.js
@@ -1,23 +1,32 @@
 import catalogo from "../../data/catalogo";
-prendas = catalogo[estilo] || [];
+
 export default async function handler(req, res) {
   try {
     const { imageUrl, estilo } = req.body;
 
+    console.log("ESTILO:", estilo);
+
     let outfitImg = "";
-    let prendas = [];
-    let nombreEstilo = "";
 
+    if (estilo === "urbano") {
+      outfitImg = "https://images.unsplash.com/photo-1520975916090-3105956dac38";
+    }
 
+    if (estilo === "elegante") {
+      outfitImg = "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce";
+    }
+
+    const prendas = catalogo[estilo] || [];
 
     res.status(200).json({
-      image: imageUrl, // 👈 tu imagen
-      outfit: outfitImg, // 👈 outfit referencia
-      estilo: nombreEstilo,
+      image: imageUrl,
+      outfit: outfitImg,
+      estilo: estilo,
       prendas: prendas
     });
 
   } catch (error) {
+    console.error("ERROR API:", error);
     res.status(500).json({ error: "error" });
   }
 }
