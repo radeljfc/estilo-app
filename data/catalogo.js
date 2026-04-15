1bd7ccd9274548ca5044d4387ced6127c486821f
radeljfc <radeljfc@icloud.com>
2026-04-14 20:09:54 -0500

    Create catalogo.js

---
+++ data/catalogo.js
@@ -0,0 +1,31 @@
+export const catalogo = {
+  urbano: [
+    {
+      nombre: "Hoodie negro",
+      precio: "S/ 80",
+      imagen: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab",
+      link: "https://wa.me/51999999999?text=Quiero%20el%20hoodie"
+    },
+    {
+      nombre: "Jeans rotos",
+      precio: "S/ 100",
+      imagen: "https://images.unsplash.com/photo-1541099649105-f69ad21f3246",
+      link: "https://wa.me/51999999999?text=Quiero%20los%20jeans"
+    }
+  ],
+
+  elegante: [
+    {
+      nombre: "Blazer beige",
+      precio: "S/ 120",
+      imagen: "https://images.unsplash.com/photo-1593030761757-71fae45fa0e7",
+      link: "https://wa.me/51999999999?text=Quiero%20el%20blazer"
+    },
+    {
+      nombre: "Camisa blanca",
+      precio: "S/ 60",
+      imagen: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf",
+      link: "https://wa.me/51999999999?text=Quiero%20la%20camisa"
+    }
+  ]
+};
