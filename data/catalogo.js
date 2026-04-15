e7806dd32a8c4277200a7f88a1d0fb518da368a0
radeljfc <radeljfc@icloud.com>
2026-04-14 21:50:01 -0500

    Update catalogo.js

--- data/catalogo.js
+++ data/catalogo.js
@@ -36,7 +36,7 @@ const catalogo = {
     {
       nombre: "Pantalón slim",
       precio: "S/ 180",
-      imagen: "https://images.unsplash.com/photo-1528701800489-20be3c9c1d2b?w=500"",
+      imagen: "https://images.unsplash.com/photo-1528701800489-20be3c9c1d2b?w=500",
       link: "https://wa.me/51914680763?text=Quiero%20la%20opcion"
     }
   ]
